5bc38f5162bffd7ee27c37b6a9bb3ae3ed2d016d
Calvin Smith <calvin@all-hands.dev>
2026-01-06 12:01:22 -0700

    more docs

--- openhands-sdk/openhands/sdk/context/view/properties/base.py
+++ openhands-sdk/openhands/sdk/context/view/properties/base.py
@@ -1,8 +1,7 @@
 from abc import ABC, abstractmethod
 
-from openhands.sdk.context.view.event_mappings import EventMappings
 from openhands.sdk.context.view.manipulation_indices import ManipulationIndices
-from openhands.sdk.event.base import LLMConvertibleEvent
+from openhands.sdk.event.base import Event, LLMConvertibleEvent
 from openhands.sdk.event.types import EventID
 
 
@@ -22,21 +21,34 @@ class ViewPropertyBase(ABC):
 
     @abstractmethod
     def enforce(
-        self, events: list[LLMConvertibleEvent], event_mappings: EventMappings
+        self, current_view_events: list[LLMConvertibleEvent], all_events: list[Event]
     ) -> set[EventID]:
         """Enforce the property on a list of events.
 
         Args:
-            events: A list of LLMConvertibleEvent objects to enforce the property on.
+            current_view_events: A list of events currently in the view.
+            all_events: A list of all Event objects in the conversation. Useful for
+                properties that need to reference events outside the current view.
 
         Returns:
-            A set of EventID objects to be removed to enforce the property.
+            A set of EventID objects to be removed from the current view to enforce the
+            property.
         """
         pass
 
     @abstractmethod
     def manipulation_indices(
-        self, events: list[LLMConvertibleEvent], event_mappings: EventMappings
+        self, current_view_events: list[LLMConvertibleEvent], all_events: list[Event]
     ) -> ManipulationIndices:
-        """Get manipulation indices for the property on a list of events."""
+        """Get manipulation indices for the property on a list of events.
+        
+        Args:
+            current_view_events: A list of events currently in the view.
+            all_events: A list of all Event objects in the conversation. Useful for
+                properties that need to reference events outside the current view.
+
+        Returns:
+            A ManipulationIndices object defining where the view can be manipulated
+            while maintaining the property.
+        """
         pass
